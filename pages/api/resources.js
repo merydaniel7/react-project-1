8bea488e58570917637bc1169b80212ed890d7e0
merydaniel7 <merydaniel7@gmail.com>
2021-07-12 15:30:48 +0200

    Add: resource detail page

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,17 +1,24 @@
+import axios from "axios";
 
-
-export default async function(req, resp) {
+export default async function(req, res) {
     if (req.method === "GET") {
         const dataResponse = await fetch("http://localhost:3001/api/resources");
         const data = await dataResponse.json();
 
-        return resp.send(data)
+        return res.send(data)
     }
     if (req.method === "POST") {
         const { title, description, link, timeToFinish, priority } = req.body;
         if (! title || !description || !link || !timeToFinish || !priority) {
-            return resp.status(422).send("Data are missing!")
+            return res.status(422).send("Data are missing!")
         }
-        return resp.send("Data has been received!")
+    try {
+        const axiosResponse = await axios.post("http://localhost:3001/api/resources", req.body);
+        return res.send(axiosResponse.data);
+    } catch {
+        return res.status(422).send("Data cannot be stored!");
+    }    
+    
+
     }
 }
